e723f2ed608431f25a7965a6c500591e76663864
minkhant <mink12392@gmail.com>
2023-04-05 17:33:52 +0630

    Third Day

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -1,15 +1,73 @@
 <?php
-
 class User extends Controller{
     public function __construct()
     {
         
     }
     public function register(){
+        if($_SERVER['REQUEST_METHOD']=="POST"){
+            $_POST=filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data=[
+                "name"=>$_POST['name'],
+                "email"=>$_POST['email'],
+                "password"=>$_POST['password'],
+                "confirm_password"=>$_POST['confirm_password'],
+                "name_err"=>'',
+                "email_err"=>'',
+                "password_err"=>'',
+                "confirm_password_err"=>''
+            ];
+            if(empty($data['name'])){
+                $data['name_err']="User Name must be supply!";
+            }
+            if(empty($data['email'])){
+                $data['email_err']="Email must be supply!";
+            }
+            if(empty($data['password'])){
+                $data['password_err']="Password must be supply!";
+            }
+            if(empty($data['confirm_password'])){
+                $data['confirm_password_err']="Confirm_password must be supply!";
+            }else{
+                if($data['password'] != $data['confirm_password']){
+                    $data['confirm_password_err']="Password do not match";
+                }
+            }
+            if(empty($data['name_err']) && empty($data['email_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])){
+                echo "Already Good!";
+            }else{
+                $this->view("user/register",$data);
+            }
+        }else{
         $this->view("user/register");
+
+        }
+      
     }
     public function login(){
+        if($_SERVER['REQUEST_METHOD']=="POST"){
+            $_POST=filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data=[
+                "email"=>$_POST['email'],
+                "password"=>$_POST['password'],
+                "email_err"=>'',
+                "password_err"=>''
+            ];
+            if(empty($data['email'])){
+                $data['email_err']="Email must be supply!";
+            }
+            if(empty($data['password'])){
+                $data['password_err']="Password must be supply!";
+            }
+            if(empty($data['email_err']) && empty($data['password_err'])){
+                echo "Already Good!";
+            }else{
+                $this->view("user/login",$data);
+            }
+        }else{
         $this->view("user/login");
+
+        }
     }
 }
 
--- app/views/user/register.php
+++ app/views/user/register.php
@@ -6,22 +6,27 @@
             <div class="card bg-light p-5">
                 <!-- register form start -->
                 <h1 class="text-center text-info mb-3">Register Form</h1>
-                <form>
+                <form method="post" action="<?php echo URLROOT."user/register";  ?>">
                     <div class="mb-3">
                         <label for="username" class="form-label">Username</label>
-                        <input type="text" class="form-control rounded-0" id="username" aria-describedby="username" name="username" >
+                        <input type="text" class="form-control rounded-0 <?php echo !empty($data["name_err"]) ? 'is-invalid' : '';?>" id="username" aria-describedby="username" name="name" >
+                        <span class="text-danger"><?php echo !empty($data["name_err"]) ? $data["name_err"] : '';?></span>
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" class="form-control rounded-0" id="email" aria-describedby="email" name="email">
+                        <input type="email" class="form-control rounded-0 <?php echo !empty($data["email_err"]) ? 'is-invalid' : '';?>" id="email" aria-describedby="email" name="email">
+                        <span class="text-danger"><?php echo !empty($data["email_err"]) ? $data["email_err"] : '';?></span>
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control rounded-0" id="password" aria-describedby="password" name="password">
+                        <input type="password" class="form-control rounded-0 <?php echo !empty($data["password_err"]) ? 'is-invalid' : '';?>" id="password" aria-describedby="password" name="password">
+                        <span class="text-danger"><?php echo !empty($data["password_err"]) ? $data["password_err"] : '';?></span>
                     </div>
+                 
                     <div class="mb-3">
                         <label for="confirm_password" class="form-label">Confirm Password</label>
-                        <input type="password" class="form-control rounded-0" id="confirm_password" aria-describedby="password" name="confirm_password">
+                        <input type="password" class="form-control rounded-0 <?php echo !empty($data["confirm_password_err"]) ? 'is-invalid' : '';?>" id="confirm_password" aria-describedby="password" name="confirm_password">
+                        <span class="text-danger"><?php echo !empty($data["confirm_password_err"]) ? $data["confirm_password_err"] : '';?></span>
                     </div>
                     <div class="row justify-content-between no-gutter" style=" float: left;flex-wrap: nowrap;">
                         <a href="<?php echo URLROOT.'user/login';   ?>" class="link-warning" style="    text-decoration: none;">Already Register, Please Login!</a>
